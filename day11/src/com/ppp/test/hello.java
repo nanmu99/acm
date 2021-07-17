f977677fd2dcaece5b6c351408deb335e1188634
nanmu99 <1945465209@qq.com>
2021-07-17 18:52:49 +0800

    增加一个新的文件

---
+++ day11/src/com/ppp/test/hello.java
@@ -0,0 +1,11 @@
+package com.ppp.test;
+
+/**
+ * @auther ppp
+ * @create 2021-07-17 18:51
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
